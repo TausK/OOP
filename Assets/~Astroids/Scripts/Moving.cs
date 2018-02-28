95bbb8c61eab2a10459016094cda268a22eb56dc
tausifur.khan2 <tausifur.khan2@600900B211DE416>
2018-02-28 11:56:05 +1100

    Scripts & Scene New

---
+++ Assets/~Astroids/Scripts/Moving.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Astroids
+{
+
+    public class Moving : MonoBehaviour
+    {
+        #region
+        [Header("Speeds")]
+        public float acceleration = 5f;
+        [Space(5)]
+        public float rotationSpeed = 360f;
+        [Space(5)]
+        public float maxVelocity = 3f;
+
+        private Rigidbody2D rb;
+        
+        #endregion
+
+        // Use this for initialization
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            LimitVelocity();
+        }
+
+        void LimitVelocity()
+        {
+            Vector3 vel = rb.velocity; // Velocity = rigidbodys velocity
+            if(vel.magnitude > maxVelocity) //if vector3(player) magnitude > player Velocity Then
+            {
+                vel = vel.normalized * maxVelocity; // reduce player velocity to normal acceleration
+            }
+            //Applies the copied varied to velocity
+            rb.velocity = vel; // Hence rigidbody of player will = normalisation of the acceleration
+        }
+
+        public void Accelerate(Vector3 direction)
+        {
+            rb.AddForce(direction * acceleration);
+        }
+
+        public void Rotate(float angleOffset)
+        {
+            rb.rotation -= angleOffset * rotationSpeed * Time.deltaTime;
+        }
+
+    }
+}
---
+++ Assets/~Astroids/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Astroids
+{
+    public class PlayerController : MonoBehaviour
+    {
+        #region Variables
+        public Moving movement; // attaching Moving script to PlayerController and naming it movement
+
+        #endregion
+ 
+        // Update is called once per frame
+        void Update()
+        {
+            float inputV = Input.GetAxis("Vertical"); // Move Vertically
+            float inputH = Input.GetAxis("Horizontal");// Move Horizontally
+            // Check if up arrows
+            if(inputV > 0) // vertical direction > 0
+            {
+
+                // Accelerate player
+                movement.Accelerate(transform.up); // Player will move up 
+            }
+            //Rotate in correct direction
+            movement.Rotate(inputH); // Simplified version of having if statements
+                //Check if horizontal axis (left or right arrow) is pressed         
+        }
+        
+    }
+}
